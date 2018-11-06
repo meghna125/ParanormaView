b029c9ec5429678fd75c0cb0e78edecce1df419d
HP <meghnaagarwal2696@gmail.com>
2018-11-06 19:45:57 +0530

    Initial commit

---
+++ app/src/main/java/com/meghna125/paranormaview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.meghna125.paranormaview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gjiazhe.panoramaimageview.GyroscopeObserver;
+import com.gjiazhe.panoramaimageview.PanoramaImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private GyroscopeObserver gyroscopeObserver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gyroscopeObserver = new GyroscopeObserver();
+        gyroscopeObserver.setMaxRotateRadian(Math.PI/9);
+
+        PanoramaImageView panoramaImageView = (PanoramaImageView)findViewById(R.id.panorma_image_view);
+        panoramaImageView.setGyroscopeObserver(gyroscopeObserver);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gyroscopeObserver.register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gyroscopeObserver.unregister();
+    }
+}
